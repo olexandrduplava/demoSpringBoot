b088f6dc92bc90e80d19be71345233cd331035e7
olexandrduplava <olexandrduplava@gmail.com>
2022-06-20 18:24:10 +0300

    Added basic Security
    Added  basic security with 2 roles: "ADMIN" and "USER"
    Also added few pages to login and show pages.
    Added verification if it's admin and if yes, he have acces to see users and articles.
    Postman wanted credits

---
+++ Project/src/main/java/com/demoSrping/demoSpring/config/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.demoSrping.demoSpring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/users", "/articles").hasAnyRole("ADMIN")
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("user")
+                        .roles("USER")
+                        .build();
+
+        UserDetails user2 =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("admin")
+                        .roles("ADMIN")
+                        .build();
+        return new InMemoryUserDetailsManager(user,user2);
+    }
+}
--- Project/src/test/java/com/demoSrping/demoSpring/controller/UserControllerTest.java
+++ Project/src/test/java/com/demoSrping/demoSpring/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.demoSrping.demoSpring.controller;
 import com.demoSrping.demoSpring.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,6 +16,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -64,5 +67,15 @@ class UserControllerTest {
         mockMvc.perform(delete("/users/{id}", 8)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getAllUsersWithArticlesMoreThan3() throws Exception {
+        mockMvc.perform(get("/users/getWithThree"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$").value("name1"));
+
     }
 }
